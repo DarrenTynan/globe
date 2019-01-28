57ab7d98c4048634fe1713a135855480eb77f2f3
Darren Tynan <darren.tynan@gmail.com>
2019-01-28 21:03:38 +0000

    Babel is working! Woop, woop!  And the production code is minified and converted to es2015.

--- webpack.config.js
+++ webpack.config.js
@@ -13,32 +13,13 @@ module.exports = {
         use: [
           'style-loader',
           'css-loader'
-        ],
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
       }
     ]
-  },
-  optimization: {
-    minimize: true,
-    splitChunks: {
-      chunks: 'async',
-      minSize: 30000,
-      maxSize: 0,
-      minChunks: 1,
-      maxAsyncRequests: 5,
-      maxInitialRequests: 3,
-      automaticNameDelimiter: '~',
-      name: true,
-      cacheGroups: {
-        vendors: {
-          test: /[\\/]node_modules[\\/]/,
-          priority: -10
-        },
-        default: {
-          minChunks: 2,
-          priority: -20,
-          reuseExistingChunk: true
-        }
-      }
-    }
   }
 };
